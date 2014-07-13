e373bd97c13ecb465190d9f2c326b6018aa2e50a
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-13 00:00:31 -0700

    Changed call to ionization fraction.
    Didn’t work in other modules.

--- ion/effective_mobility.py
+++ ion/effective_mobility.py
@@ -5,7 +5,7 @@ def effective_mobility(obj, pH, I=0):
     else:
         actual_mobility = obj.robinson_stokes_mobility(I)
 
-    i_frac = ionization_fraction(obj, pH, I)
+    i_frac = obj.ionization_fraction(pH, I)
     effective_mobility = sum([f*m for (f, m) in zip(i_frac, actual_mobility)])
 
     return effective_mobility
