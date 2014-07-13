4d7572938a2a3bd3950f8ae4491769d85b037d72
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-13 15:52:51 -0700

    Added dielectric and viscosity.
    Dielectric and viscosity are temperature adjustment functions.

---
+++ Solution/dielectric.py
@@ -0,0 +1,10 @@
+def dielectric(T=25):
+    """Return the dielectric constant of water at a specified temperature.
+
+    The temperature should be specified in celcius. It is internally converted
+    to Kelvin. Correlation is based on the CRC handbook, as implimented in
+    STEEP.
+    """
+    Tk = T+273.15
+    d = (2.414e-5)*10**(247.8/(Tk-140))
+    return d
---
+++ Solution/viscosity.py
@@ -0,0 +1,10 @@
+def viscosity(T=25):
+    """Return the viscosity of water at the specified temperature.
+
+    The temperature should be specified in celcius. The function internally
+    converts the temperature to Kelvin. Correlation is based on Fox and
+    McDonald's Intro to FLuid Mechanics, as implimented in STEEP. 
+    """
+    Tk = T+273.15
+    v = (2.414e-5)*10**(247.8/(Tk-140))
+    return v
