074578baa6805eed617ffded70be50485d0068f8
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-13 00:03:42 -0700

    Clarified equation.

--- solution/calc_I.py
+++ solution/calc_I.py
@@ -9,9 +9,9 @@ def calc_I(obj, pH, I_guess=0):
     """
     # For each ion, add the contribution to ionic strength to the sum.
     I = sum([c * sum(
-        [z**2*f for (z, f) in
+        [z**2*f/2 for (z, f) in
          zip(ionp.z, ionp.ionization_fraction(pH, I_guess))])
-        for c, ionp in zip(obj.concentrations, obj.ions)])/2
+        for c, ionp in zip(obj.concentrations, obj.ions)])
 
     # Add the ionic strength due to water dissociation.
     I = I+(obj.cH(pH) + obj.cOH(pH, I_guess))/2
