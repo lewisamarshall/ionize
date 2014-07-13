027046dff17547798bdfccf44120d7dcf0a23094
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-13 00:01:42 -0700

    Reduced the acid insult concentration.

--- solution/buffering_capacity.py
+++ solution/buffering_capacity.py
@@ -1,3 +1,6 @@
+from Ion import Ion
+
+
 def buffering_capacity(obj):
     """Return the buffering capacity of the solution.
 
@@ -5,12 +8,13 @@ def buffering_capacity(obj):
     capacity by finding the derivative of the pH with respect to
     the addition of an acid insult at small concentration.
     """
-    # Find the smallest concentration in the solution.
-    c = [cp for cp in obj.concentrations() if cp > 0]
-    c = 0.1*min(c)
+    # Remove any ions at concentration 0.
+    c = [cp for cp in obj.concentrations if cp > 0]
+    # Choose a concentration 1% the lowest ion concentration.
+    c = 0.01*min(c)
 
     # Add an acid insult at 1% the lowest concentration in the solution.
-    new_sol = obj.add_ion(Ion('Acid Insult', -1, -2, -1), c)
+    new_sol = obj.add_ion([Ion('Acid Insult', -1, -2, -1)], [c])
 
     # Find the slope of the pH.
     Cb = abs(c/(obj.pH-new_sol.pH))
