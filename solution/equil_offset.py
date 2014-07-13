9581e9c72751fcd2b8b212defe550b5a1fcfdd95
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-13 00:04:19 -0700

    Docstring update.

--- solution/equil_offset.py
+++ solution/equil_offset.py
@@ -1,9 +1,9 @@
 def equil_offset(obj, I_i):
     """Return the error in ionic strength.
 
-    Takes an ionic strength, then uses it to calculate
-    a new ionic strenght using the new equilibrum coefficents.
-    find_equilibrium finds the root of this function.
+    Takes an ionic strength, then uses it to calculate a new ionic strength
+    using the new equilibrum coefficents. find_equilibrium finds the root of
+    this function.
     """
     pH = obj.calc_pH(I_i)
     I_f = obj.calc_I(pH, I_i)
