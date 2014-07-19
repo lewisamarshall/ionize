80ea25a115c22b15e94c4ece4618b39b4b788851
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-19 15:42:43 -0700

    Simplified L.
    Made L use list comprehensions instead of loops. Gave it a self-test.

--- ionize/Ion/L.py
+++ ionize/Ion/L.py
@@ -1,4 +1,4 @@
-from numpy import cumprod
+from numpy import cumprod, prod
 
 
 def L(obj, I=0):
@@ -8,19 +8,24 @@ def L(obj, I=0):
     It can use ionic strength correction if an ionic strength is specified.
     Otherwise, it uses uncorrected acidity coefficients.
     """
-    L = obj.z0()
     Ka = obj.Ka_eff(I)
     index_0 = obj.z0().index(0)
-    L[index_0] = 1
+    Ka.insert(index_0, 1)
 
-    if index_0 is not 0:
-        for i in range(index_0-1, -1, -1):
-            L[i] = L[i+1]*Ka[i]
+    L = [prod(Ka[i:index_0]) for i in range(len(Ka)) if i < index_0] +\
+        [1/prod(Ka[index_0:i+1]) for i in range(len(Ka)) if i >= index_0]
 
-    if index_0 is not len(L)-1:
-        for i in range((index_0+1), len(L)):
-            L[i] = L[i-1]/Ka[i-1]
-
-    # print L
-    # print [p/cumprod(Ka)[index_0] for p in cumprod(Ka)]
     return L
+
+if __name__ == '__main__':
+    def a():
+        pass
+
+    def z0():
+        return[-1, 0, 1, 2]
+
+    def Ka_eff(I):
+        return [4.677351412871981e-10, 9.120108393559096e-07, 0.01]
+    a.Ka_eff = Ka_eff
+    a.z0 = z0
+    print L(a)
