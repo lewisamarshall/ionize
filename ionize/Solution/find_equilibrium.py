b232b91803411125b958d63523ce6d1875f91655
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-07-20 12:36:05 -0700

    Improved root finding in find_equilibrium.

--- ionize/Solution/find_equilibrium.py
+++ ionize/Solution/find_equilibrium.py
@@ -1,4 +1,5 @@
-from scipy.optimize import newton
+from scipy.optimize import newton, brentq
+import warnings
 
 
 def find_equilibrium(obj):
@@ -12,8 +13,21 @@ def find_equilibrium(obj):
     # Generate an initial ionic strength guess without  activity corrections
     I = obj.calc_I(obj.calc_pH())
 
-    # Iterate to find the true ionic strength.
-    I = newton(obj.equil_offset, I)
+    # Try to bound the true answer.
+    b1 = obj.equil_offset(0)
+    b2 = obj.equil_offset(2*I)
+
+    # If the answer is in the bound, use brentq. Otherwise, use newton.
+    # If brentq doesn't converge, use newton.
+    if ((b1 < 0) ^ (b2 < 0)):
+        I, r = brentq(obj.equil_offset, 0, 2*I, full_output=True)
+        if not r.converged:
+            I = newton(obj.equil_offset, I)
+            warnings.warn('Couldn\'t use the brentq method. Using newton.')
+
+    else:
+        I = newton(obj.equil_offset, I)
+        warnings.warn('Couldn\'t use the brentq method. Using newton.')
 
     # Use this final ionic strength to find the correct pH.
     pH = obj.calc_pH(I)
