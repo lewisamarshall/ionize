b82378bf3755d2bbf6166f8d355ec5c5ee4aae53
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-08-08 23:55:35 -0700

    Added scipy.optimize to mock.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sys
 import os
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.optimize']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
