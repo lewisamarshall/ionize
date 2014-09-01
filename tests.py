cba30a26dfb0ddcdf74281b83a9f71fd6f33f80d
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-08-31 23:34:02 -0700

    unit tests added

---
+++ tests.py
@@ -0,0 +1,32 @@
+import ionize
+import unittest
+
+
+class TestIon(unittest.TestCase):
+
+    def setUp(self):
+        self.db = ionize.get_db()
+
+    def test_import(self):
+        for ion_name in self.db.keys():
+            ion = ionize.load_ion(ion_name)
+            ion.molar_conductivity(7, 0.1)
+
+
+class TestSolution(unittest.TestCase):
+
+    def setUp(self):
+        self.hcl = ionize.load_ion('hydrochloric acid')
+        self.tris = ionize.load_ion('tris')
+
+    def test_titration(self):
+        c_tris = 1.0
+        pH_old = 14
+        n = 100
+        c_hcl_set = [c_tris * i * 2.0 / n for i in range(n)]
+        for c_hcl in c_hcl_set:
+            buf = ionize.Solution([self.tris, self.hcl], [c_tris, c_hcl])
+            self.assertTrue(buf.pH < pH_old)
+
+if __name__ == '__main__':
+    unittest.main()
