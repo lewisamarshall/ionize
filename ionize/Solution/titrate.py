4c9f4e71f3ba97af059cc86d0e24fc66368f925d
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-09-23 21:25:21 -0700

    Change to titrate.
    Allow titrate to work with argument-free Solution methods.
    Remove “property” as a keyword, and switch to “titration_property”.

--- ionize/Solution/titrate.py
+++ ionize/Solution/titrate.py
@@ -1,8 +1,9 @@
 from scipy.optimize import newton, brentq
+import numbers
 from ..Ion import Ion
 
 
-def titrate(self, titrant, target, property='pH', return_c=False):
+def titrate(self, titrant, target, titration_property='pH', return_c=False):
     """Return a Solution titrated to the target pH using the titrant.
 
     Titrate uses root finding to determine the concentration of titrant
@@ -12,10 +13,16 @@ def titrate(self, titrant, target, property='pH', return_c=False):
     To titrate to a target property other than pH, simply set the property
     to a property of the Solution class.
     """
-
-    min_func = lambda c: (self + (titrant, c)).__dict__[property]-target
+    att = getattr(self, titration_property)
+    if isinstance(att, numbers.Number):
+        min_func = lambda c:\
+            (self + (titrant, c)).__dict__[titration_property]-target
+    else:
+        min_func = lambda c: \
+            getattr(self + (titrant, c), titration_property)()-target
 
     c, r = brentq(min_func, 0, 55, full_output=True)
+
     if r.converged:
         if return_c:
             return c
