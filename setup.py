8215fc8b4e6ca274172074734e6bd137df617e6b
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-10-16 14:51:16 -0700

    New Release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ionize',
-      version='0.5.2',
+      version='0.5.3',
       author='Lewis A. Marshall',
       author_email='lewis.a.marshall@gmail.com',
       url="http://lewisamarshall.github.io/ionize/",
