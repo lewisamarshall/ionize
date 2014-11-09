a603d7776aff7cc569eb3b07c1aac3c92631d6f5
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-11-08 17:46:36 -0800

    Partially ionized species diffusivity bug fix.

--- ionize/Ion/diffusivity.py
+++ ionize/Ion/diffusivity.py
@@ -1,11 +1,12 @@
 def diffusivity(self, pH):
     """Return the diffusivity of the species at a specified pH.
 
-    The diffusivity is returned in units of m^2/s. 
+    The diffusivity is returned in units of m^2/s.
     """
     diffusivity = sum([m * f / float(z) for
                       m,f,z in zip(self.absolute_mobility,
                                    self.ionization_fraction(pH),
                                    self.z
                                    )]) * self._kB * (self.T + 273.15)
+    diffusivity/=sum(self.ionization_fraction(pH))
     return diffusivity
