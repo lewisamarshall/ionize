7cadade03ff3aa7f6e560cedab61ce153e819f01
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-03-03 23:54:14 -0800

    Formatted diffusivity.

--- ionize/Ion/diffusivity.py
+++ ionize/Ion/diffusivity.py
@@ -4,9 +4,9 @@ def diffusivity(self, pH):
     The diffusivity is returned in units of m^2/s.
     """
     diffusivity = sum([m * f / float(z) for
-                      m,f,z in zip(self.absolute_mobility,
-                                   self.ionization_fraction(pH),
-                                   self.z
-                                   )]) * self._kB * (self.T + 273.15)
-    diffusivity/=sum(self.ionization_fraction(pH))
+                      m, f, z in zip(self.absolute_mobility,
+                                     self.ionization_fraction(pH),
+                                     self.z
+                                     )]) * self._kB * (self.T + 273.15)
+    diffusivity /= sum(self.ionization_fraction(pH))
     return diffusivity
