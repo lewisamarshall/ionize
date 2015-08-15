5e99a7a09752dc66d186d35e40f736e6c8696bf6
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-15 12:47:27 -0700

    Moved Aqueous from folder to file.

--- ionize/Aqueous.py
+++ ionize/Aqueous.py
@@ -5,11 +5,13 @@ class Aqueous(object):
 
     """Describe the properties of water."""
 
-    def __init__(self):
-        """Initialize."""
-        pass
+    _Kw_ref = 1E-14        # Water equilibrium constant [mol^2]
+    _dHw = 55.815e3
+    _dCpw = -224
+    _T_ref = 25.
+    _R = 8.31              # Universal gas const.       [J/mol*K]
 
-    def _dielectric(self, T=None):
+    def dielectric(self, T=None):
         """Return the dielectric constant of water at a specified temperature.
 
         The temperature should be specified in Celcius. Correlation is based on
@@ -19,7 +21,7 @@ class Aqueous(object):
         d = 249.21 - 0.79069*Tk + 0.72997e-3*Tk**2
         return d
 
-    def _viscosity(self, T=None):
+    def viscosity(self, T=None):
         """Return the viscosity of water at the specified temperature.
 
         The temperature should be specified in celcius. The function internally
@@ -29,3 +31,17 @@ class Aqueous(object):
         Tk = T+273.15
         v = (2.414e-5)*10**(247.8/(Tk-140))
         return v
+
+    def dissociation(self, T=None):
+        """Return the dissociation constant of water."""
+        if not T:
+            return self._Kw_ref
+
+        pKw_ref = -log10(self._Kw_ref)
+        T_ref = self._T_ref + 273.15
+        T += 273.15
+        pKw = pKw_ref -\
+            (self._dHw/2.303/self._R)*(1.0/T_ref - 1.0/T) -\
+            (self._dCpw/2.303/self._R)*(T_ref/T-1.0+log(T/T_ref))
+        Kw = 10.0**(-pKw)
+        return Kw
