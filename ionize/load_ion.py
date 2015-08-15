238f3e47ad45b47dbb84bd276d08f2bc58fd300c
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-15 14:03:05 -0700

    Simplify load_ion.

--- ionize/load_ion.py
+++ ionize/load_ion.py
@@ -1,18 +1,6 @@
 import warnings
-from Ion import Ion
+from .Ion import Ion
 from get_db import get_db
-from load_nightingale import load_nightingale
-
-
-def deserialize_ion(serial):
-        return Ion(serial['name'],
-                   serial['z'],
-                   serial['pKa_ref'],
-                   serial['absolute_mobility_ref'],
-                   serial['dH'],
-                   serial['dCp'],
-                   serial['nightingale_function'],
-                   T=25.0, T_ref=25.0)
 
 
 def load_ion(ion_name, solvation=True):
@@ -27,11 +15,11 @@ def load_ion(ion_name, solvation=True):
     ion_list = get_db()  # Open the database.
     if ion_name in ion_list.keys():
         ion_entry = ion_list[ion_name]
-
-        return deserialize_ion(ion_entry)
+        ion_entry.pop('__ion__')
+        return Ion(**ion_entry)
     else:
         # warnings.warn('Ion not found in database.)
-        raise NameError(ion_name)
+        raise NameError('Ion {} not found in database.'.format(ion_name))
 
 if __name__ == "__main__":
     ions = get_db()
