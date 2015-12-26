285d0cf307dd6e28cf38c2a646e1a7bed74e39d4
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-12-25 20:07:33 -0500

    Started adding manning model to NucleicAcid.

--- ionize/PolyIon/NucleicAcid.py
+++ ionize/PolyIon/NucleicAcid.py
@@ -1,3 +1,5 @@
+from math import exp, log, pi
+
 from .PolyIon import PolyIon
 from ..Ion import fixed_state
 
@@ -34,8 +36,13 @@ class NucleicAcid(PolyIon):
         assert self.species in self.__species_options.keys()
 
     def mobility(self, pH=None, ionic_strength=None, temperature=None):
+        pH, ionic_strength, temperature = \
+                self._resolve_context(pH, ionic_strength, temperature)
         # TODO: Introduce manning condensation model
         mu = (3.75 - 1.8 * (self.size**-.6)) * 1e-8
+        k = 1./self._solvent.debye(ionic_strength, temperature)
+        # mu = 6 * pi * self.solvent.viscosity() * lb / self.friction()
+        # mu -= 2 * self.charge() * log(1 - exp(-k * lb / self.charge()))
         return mu
 
     def charge(self, pH=None, ionic_strength=None, temperature=None,
@@ -44,3 +51,6 @@ class NucleicAcid(PolyIon):
         # TODO: introduce manning condensation model
         # return self.size * (1 + self.__species_options[self.species]['duplex'])
         raise NotImplementedError
+
+    def friction(self, pH=None, ionic_strength=None, temperature=None):
+        pass
