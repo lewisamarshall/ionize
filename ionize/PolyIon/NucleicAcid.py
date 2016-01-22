36b921697309cb40e98bde32066cb87c2a8300ae
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2016-01-21 23:08:06 -0800

    Started universal tests.

--- ionize/PolyIon/NucleicAcid.py
+++ ionize/PolyIon/NucleicAcid.py
@@ -39,18 +39,23 @@ class NucleicAcid(PolyIon):
         pH, ionic_strength, temperature = \
                 self._resolve_context(pH, ionic_strength, temperature)
         # TODO: Introduce manning condensation model
-        mu = (3.75 - 1.8 * (self.size**-.6)) * 1e-8
+        # mu = (3.75 - 1.8 * (self.size**-.6)) * 1e-8
+        lb = self._solvent.bjerrum(temperature)
         k = 1./self._solvent.debye(ionic_strength, temperature)
-        # mu = 6 * pi * self.solvent.viscosity() * lb / self.friction()
-        # mu -= 2 * self.charge() * log(1 - exp(-k * lb / self.charge()))
+        mu = 6 * pi * self.solvent.viscosity() * lb / self.friction()
+        mu -= 2 * self.charge() * log(1 - exp(-k * lb / self.charge()))
         return mu
 
     def charge(self, pH=None, ionic_strength=None, temperature=None,
                moment=1):
+        pH, ionic_strength, temperature = \
+                self._resolve_context(pH, ionic_strength, temperature)
 
-        # TODO: introduce manning condensation model
-        # return self.size * (1 + self.__species_options[self.species]['duplex'])
-        raise NotImplementedError
+        return -1 * self.length() / self._solvent.bjerrum(temperature)
+
+
+    def length(self):
+        return self.size * DNA_LENGTH # TODO: Define
 
     def friction(self, pH=None, ionic_strength=None, temperature=None):
         pass
