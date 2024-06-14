87457830ea74cf2e032ab7844b75e47d92719e50
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2024-06-14 09:43:46 -0700

    Revert "Added a solution CLI."

--- ionize/__main__.py
+++ ionize/__main__.py
@@ -20,21 +20,21 @@ def ion(name):
     click.echo(Database()[name].serialize(nested=False, compact=True))
 
 
-@cli.command()
-@click.option('--serialize', '-s', is_flag=True)
-@click.argument("components", nargs=-1)
-def solution(components, serialize):
-    ions = components[::2]
-    concentrations = [float(c) for c in components[1::2]]
-    assert len(ions) == len(concentrations), "There must be a concentration for each ion."
-    sol = Solution(ions, concentrations)
-    if serialize:
-        click.echo(sol.serialize(nested=False, compact=True))
-    else:
-        click.echo('pH: {:0.4f}'.format(sol.pH))
-        click.echo('ionic strength: {:0.4f} M'.format(sol.ionic_strength))
-        click.echo('conductivity: {:0.4f} s/M'.format(sol.conductivity()))
-        click.echo('Debye length: {:0.4f} um'.format(sol.debye()*1e6))
+# @cli.command()
+# @click.option('--ion', '-i', multiple=True)
+# @click.option('--concentration', '-c', multiple=True, type=float)
+# def solution(ion, concentration):
+#     sol = Solution(ion, concentration)
+#     click.echo('pH: {:0.4f}'.format(sol.pH))
+#     click.echo('ionic strength: {:0.4f} M'.format(sol.ionic_strength))
+#     click.echo('conductivity: {:0.4f} s/M'.format(sol.conductivity()))
+#     click.echo('Debye length: {:0.4f} um'.format(sol.debye()*1e6))
+#     click.echo(dir(sol))
+#
+#
+# @cli.command()
+# def io():
+#     pass
 
 if __name__ == '__main__':
     cli()
