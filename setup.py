8ee79e433208fb2f359d745edf94825f93481a30
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2024-09-24 11:46:11 -0700

    Upversion.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # Read version from package.
 # from ionize.__version__ import __version__
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 
 setup(name='ionize',
       version=__version__,
